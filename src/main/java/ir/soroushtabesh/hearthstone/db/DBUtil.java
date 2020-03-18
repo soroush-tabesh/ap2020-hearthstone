bacd8bf8b446362be5afbed6d021decf1913b7e0
Soroush Tabesh <soroushtabesh@gmail.com>
2020-03-19 00:42:46 +0330

    Add DBUtil

---
+++ src/main/java/ir/soroushtabesh/hearthstone/db/DBUtil.java
@@ -0,0 +1,56 @@
+package ir.soroushtabesh.hearthstone.db;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class DBUtil {
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                registry = new StandardServiceRegistryBuilder()
+                        .configure()
+                        //.applySetting("",)
+                        .build();
+                MetadataSources sources = new MetadataSources(registry);
+                Metadata metadata = sources.getMetadataBuilder().build();
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null)
+                    StandardServiceRegistryBuilder.destroy(registry);
+
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null)
+            StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    public static Session openSession() {
+        return getSessionFactory().openSession();
+    }
+
+    public static boolean saveSingleObject(Object object) {
+        Transaction transaction = null;
+        try (Session session = openSession()) {
+            transaction = session.beginTransaction();
+            session.save(object);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
