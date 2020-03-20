dba493d413f179e614d90cf9033eb75d03ba2686
Soroush Tabesh <soroushtabesh@gmail.com>
2020-03-20 22:10:22 +0330

    Change seeding technique
    Stable seeding is now in Seeding.java

--- src/main/java/ir/soroushtabesh/hearthstone/Application.java
+++ src/main/java/ir/soroushtabesh/hearthstone/Application.java
@@ -2,28 +2,21 @@ package ir.soroushtabesh.hearthstone;
 
 import ir.soroushtabesh.hearthstone.cli.CLIManager;
 import ir.soroushtabesh.hearthstone.cli.activities.Collections;
+import ir.soroushtabesh.hearthstone.cli.activities.StartPage;
 import ir.soroushtabesh.hearthstone.controllers.PlayerManager;
-import ir.soroushtabesh.hearthstone.models.beans.*;
-import ir.soroushtabesh.hearthstone.models.beans.cards.HeroPower;
-import ir.soroushtabesh.hearthstone.models.beans.cards.Minion;
-import ir.soroushtabesh.hearthstone.models.beans.scripts.Dummy;
-import ir.soroushtabesh.hearthstone.util.DBUtil;
-import ir.soroushtabesh.hearthstone.util.HashUtil;
 import ir.soroushtabesh.hearthstone.util.Logger;
+import ir.soroushtabesh.hearthstone.util.Seeding;
 
 public class Application {
     public static void main(String[] args) {
-//        initiate();
-//        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
-//
-//        Logger.log("application", "start");
-//        CLIManager cliManager = CLIManager.getInstance();
-//        cliManager.initializeProcessor();
-//        cliManager.startActivity(new StartPage());
-//        cliManager.fireUp();
-//        Logger.log("application", "shutdown");
-
-        tester();
+        Seeding.initiate();
+        Logger.log("application", "start");
+        CLIManager cliManager = CLIManager.getInstance();
+        cliManager.initializeProcessor();
+        cliManager.startActivity(new StartPage());
+        cliManager.fireUp();
+        Logger.log("application", "shutdown");
+        //tester();
         /*
         for deploy:
         1- enable banner
@@ -39,56 +32,6 @@ public class Application {
         PlayerManager.getInstance().authenticate("akbar", "akbar");
         cliManager.startActivity(new Collections());
         cliManager.fireUp();
-    }
-
-    public static void initiate() {
-        Logger.log("test", "test", Log.Severity.WARNING);
-        Player player = new Player("akbar", HashUtil.hash("akbar"));
-        Script script = new Dummy();
-        HeroPower heroPower = new HeroPower();
-        Hero hero = new Hero();
-        Deck deck = new Deck(hero, player);
-        Minion minion = new Minion();
-        Minion minion1 = new Minion();
-
-        player.addDeck(deck);
-        player.addOpenHero(hero);
-        player.addOwnedCard(minion);
-        player.addOwnedCard(minion1);
-
-        hero.addSpecialCard(minion);
-        hero.setHeroClass(Hero.HeroClass.DRUID);
-        hero.setHeroPower(heroPower);
-        hero.setHp(40);
-        hero.setName("Akbar");
-        hero.setSpecialPower(script);
-
-        deck.addCard(minion);
-        deck.addCard(minion1);
-
-        minion.setCard_name("Ghozmit");
-        minion.setAttackPower(2);
-        minion.setHp(5);
-        minion.setMinionClass(Minion.MinionClass.BEAST);
-        minion.setDescription("Khafan");
-        minion.setHeroClass(hero.getHeroClass());
-        minion.setMana(3);
-        minion.setPrice(12);
-        minion.setRarity(Card.Rarity.RARE);
-        minion.setScript(script);
-
-        minion1.setCard_name("Peshgel");
-        minion1.setAttackPower(3);
-        minion1.setHp(1);
-        minion1.setMinionClass(Minion.MinionClass.BEAST);
-        minion1.setDescription("Oskol");
-        minion1.setHeroClass(hero.getHeroClass());
-        minion1.setMana(1);
-        minion1.setPrice(3);
-        minion1.setRarity(Card.Rarity.EPIC);
-        minion1.setScript(script);
-
-        DBUtil.pushSingleObject(player);
     }
 
 }
---
+++ src/main/java/ir/soroushtabesh/hearthstone/util/Seeding.java
@@ -0,0 +1,75 @@
+package ir.soroushtabesh.hearthstone.util;
+
+import ir.soroushtabesh.hearthstone.models.beans.*;
+import ir.soroushtabesh.hearthstone.models.beans.cards.Minion;
+import ir.soroushtabesh.hearthstone.models.beans.scripts.Dummy;
+import ir.soroushtabesh.hearthstone.models.beans.scripts.HeroPower;
+import org.hibernate.Session;
+
+public class Seeding {
+    public static void initiate1() {
+        Logger.log("test", "test", Log.Severity.WARNING);
+        try (Session session = DBUtil.openSession()) {
+            Player player = new Player("akbar", HashUtil.hash("akbar"));
+            Script script = new Dummy();
+            HeroPower heroPower = new HeroPower();
+            Hero hero = new Hero();
+            Deck deck = new Deck(hero, player);
+            Minion minion = new Minion();
+            Minion minion1 = new Minion();
+
+            DBUtil.pushSingleObject(player, session);
+            DBUtil.pushSingleObject(script, session);
+            DBUtil.pushSingleObject(heroPower, session);
+            DBUtil.pushSingleObject(hero, session);
+            DBUtil.pushSingleObject(deck, session);
+            DBUtil.pushSingleObject(minion, session);
+            DBUtil.pushSingleObject(minion1, session);
+
+            player.addDeck(deck);
+            player.addOpenHero(hero);
+            player.addOwnedCard(minion);
+            player.addOwnedCard(minion1);
+
+            hero.addSpecialCard(minion);
+            hero.setHeroClass(Hero.HeroClass.DRUID);
+            hero.setHeroPower(heroPower);
+            hero.setHp(40);
+            hero.setName("Akbar");
+            hero.setSpecialPower(script);
+
+            deck.addCard(minion);
+            deck.addCard(minion1);
+
+            minion.setCard_name("Ghozmit");
+            minion.setAttackPower(2);
+            minion.setHp(5);
+            minion.setMinionClass(Minion.MinionClass.BEAST);
+            minion.setDescription("Khafan");
+            minion.setHeroClass(hero.getHeroClass());
+            minion.setMana(3);
+            minion.setPrice(12);
+            minion.setRarity(Card.Rarity.RARE);
+            minion.setScript(script);
+
+            minion1.setCard_name("Peshgel");
+            minion1.setAttackPower(3);
+            minion1.setHp(1);
+            minion1.setMinionClass(Minion.MinionClass.BEAST);
+            minion1.setDescription("Oskol");
+            minion1.setHeroClass(hero.getHeroClass());
+            minion1.setMana(1);
+            minion1.setPrice(3);
+            minion1.setRarity(Card.Rarity.EPIC);
+            minion1.setScript(script);
+
+            DBUtil.pushSingleObject(player, session);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initiate() {
+
+    }
+}
