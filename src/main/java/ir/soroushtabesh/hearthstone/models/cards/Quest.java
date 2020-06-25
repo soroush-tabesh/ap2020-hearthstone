1725ac16e7ecbd696498c1aba40d0ec11a202ff6
Soroush Tabesh <soroushtabesh@gmail.com>
2020-06-25 18:42:56 +0430

    Remove reward script field

--- src/main/java/ir/soroushtabesh/hearthstone/models/cards/Quest.java
+++ src/main/java/ir/soroushtabesh/hearthstone/models/cards/Quest.java
@@ -1,34 +1,19 @@
 package ir.soroushtabesh.hearthstone.models.cards;
 
 import ir.soroushtabesh.hearthstone.models.Hero;
-import ir.soroushtabesh.hearthstone.models.ScriptModel;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 
 @Entity
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Quest extends Spell {
-    @ManyToOne
-    @Cascade({CascadeType.MERGE, CascadeType.SAVE_UPDATE, CascadeType.REFRESH})
-    private ScriptModel rewardScript;
 
     public Quest() {
     }
 
     public Quest(String card_name, String description, Integer mana, Hero.HeroClass heroClass, Integer price, Rarity rarity) {
         super(card_name, description, mana, heroClass, price, rarity);
-    }
-
-    public ScriptModel getRewardScript() {
-        return rewardScript;
-    }
-
-    public void setRewardScript(ScriptModel reward) {
-        this.rewardScript = reward;
     }
 
 }
