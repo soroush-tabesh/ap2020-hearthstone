984c1f2df2f7d9cf8dbad0d4f5883a4a8df63766
Soroush Tabesh <soroushtabesh@gmail.com>
2020-08-10 16:31:06 +0430

    Server mode setting

--- src/main/java/ir/soroushtabesh/hearthstone/MainServer.java
+++ src/main/java/ir/soroushtabesh/hearthstone/MainServer.java
@@ -1,7 +1,9 @@
 package ir.soroushtabesh.hearthstone;
 
+import ir.soroushtabesh.hearthstone.util.Constants;
+
 public class MainServer {
     public static void main(String[] args) {
-
+        Constants.setServerMode(true);
     }
 }
